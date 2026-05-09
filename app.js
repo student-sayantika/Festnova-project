a2c740cc5005212e2c559e6145b42e8b1adf96e6
student-sayantika <sahasayantika51@gmail.com>
2026-05-09 23:26:38 +0530

    Updated resend sender email

--- app.js
+++ app.js
@@ -36,8 +36,8 @@ const User = require('./models/user');
 // 🔹 Email
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
